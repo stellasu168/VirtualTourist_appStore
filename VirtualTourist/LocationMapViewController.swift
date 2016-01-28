4c1ac66d62bcdd3d14a46a1963d226f202bf5048
stellasu168 <millionstars@gmail.com>
2016-01-28 02:23:10 -0800

    Add long press getsture recognizer

--- VirtualTourist/LocationMapViewController.swift
+++ VirtualTourist/LocationMapViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LocationMapViewController.swift
 //  VirtualTourist
 //
 //  Created by Stella Su on 1/27/16.
@@ -7,17 +7,42 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class LocationMapViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let longPressRecogniser = UILongPressGestureRecognizer(target: self, action: "handleLongPress:")
+        
+        longPressRecogniser.minimumPressDuration = 1.0
+        mapView.addGestureRecognizer(longPressRecogniser)
+        
+        // Set the map view delegate
+        mapView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+    func handleLongPress(getstureRecognizer : UIGestureRecognizer){
+        if getstureRecognizer.state != .Began { return }
+        
+        let touchPoint = getstureRecognizer.locationInView(self.mapView)
+        let touchMapCoordinate = mapView.convertPoint(touchPoint, toCoordinateFromView: mapView)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = touchMapCoordinate
+        
+        mapView.addAnnotation(annotation)
     }
 
 

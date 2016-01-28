a87c5fd11cc962410604565d16f6eba9eab28c0b
stellasu168 <millionstars@gmail.com>
2016-01-27 16:05:37 -0800

    initial commit

---
+++ VirtualTourist/Pictures.swift
@@ -0,0 +1,13 @@
+//
+//  Pictures.swift
+//  VirtualTourist
+//
+//  Created by Stella Su on 1/27/16.
+//  Copyright © 2016 Million Stars, LLC. All rights reserved.
+//
+
+import UIKit
+
+class Pictures: NSManagedObject {
+
+}

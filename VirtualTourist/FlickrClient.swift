beba216fe0277a3af818fc4167331ed259fa0448
stellasu168 <millionstars@gmail.com>
2016-02-09 00:36:33 -0800

    Add FlickrClient.swift

---
+++ VirtualTourist/FlickrClient.swift
@@ -0,0 +1,42 @@
+//
+//  FlickrClient.swift
+//  VirtualTourist
+//
+//  Created by Stella Su on 2/9/16.
+//  Copyright © 2016 Million Stars, LLC. All rights reserved.
+//
+
+import UIKit
+import Foundation
+import CoreData
+import SystemConfiguration
+
+class FlickrClient: NSObject {
+    
+        //--------------------------------------
+        // MARK: - Properties
+        //--------------------------------------
+        
+    
+        //--------------------------------------
+        // MARK: - Connectivity
+        //--------------------------------------
+        
+    
+        //--------------------------------------
+        // MARK: - Flickr API
+        //--------------------------------------
+        
+
+        
+        //--------------------------------------
+        // MARK: - Helper functions
+        //--------------------------------------
+        
+    
+        //--------------------------------------
+        // MARK: - Shared Instance
+        //--------------------------------------
+    
+
+}

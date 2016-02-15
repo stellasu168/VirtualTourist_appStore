ecf7dc47930063a892b1103ec12b58310f156334
stellasu168 <millionstars@gmail.com>
2016-02-14 23:57:27 -0800

    Add CoreDataStackManager.swift

---
+++ VirtualTourist/CoreDataStackManager.swift
@@ -0,0 +1,13 @@
+//
+//  CoreDataStackManager.swift
+//  VirtualTourist
+//
+//  Created by Stella Su on 2/14/16.
+//  Copyright © 2016 Million Stars, LLC. All rights reserved.
+//
+
+import UIKit
+
+class CoreDataStackManager: NSObject {
+
+}

d67aaa3641ffb93a4314cf7bc7b36a8dbf42b32b
stellasu168 <millionstars@gmail.com>
2016-02-23 01:44:35 -0800

    Add PhotoCollectionViewCell.swift

---
+++ VirtualTourist/PhotoCollectionViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  PhotoCollectionViewCell.swift
+//  VirtualTourist
+//
+//  Created by Stella Su on 2/23/16.
+//  Copyright © 2016 Million Stars, LLC. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var photoView: UIImageView!
+    
+    
+    override func prepareForReuse() {
+        
+        super.prepareForReuse()
+        
+        if photoView.image == nil {
+            activityIndicator.startAnimating()
+        }
+    }
+
+}

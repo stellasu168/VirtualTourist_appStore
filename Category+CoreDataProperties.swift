b9290b4f07e442cbd1f5bda557ace48052804f92
stellasu168 <millionstars@gmail.com>
2016-03-05 17:48:18 -0800

    adding a third entity

---
+++ Category+CoreDataProperties.swift
@@ -0,0 +1,19 @@
+//
+//  Category+CoreDataProperties.swift
+//  VirtualTourist
+//
+//  Created by Stella Su on 3/5/16.
+//  Copyright © 2016 Million Stars, LLC. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension Category {
+
+    @NSManaged var categoryName: String?
+
+}
---
+++ Category.swift
@@ -0,0 +1,16 @@
+//
+//  Category.swift
+//  VirtualTourist
+//
+//  Created by Stella Su on 3/5/16.
+//  Copyright © 2016 Million Stars, LLC. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class Category: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
